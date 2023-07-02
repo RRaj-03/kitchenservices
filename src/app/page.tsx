7bf56211dc208ca5e9c1354932a5fead08641129
rraj-03 <ritikraj1369@gmail.com>
2023-07-03 01:47:24 +0530

    HOme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import Navbar from '../Components/Navbar/index'
+import { useRouter } from 'next/router'
 export default function Home() {
+  const Router = useRouter()
+  Router.replace("/Home")
   return (
     <>
     {/* <Navbar /> */}
