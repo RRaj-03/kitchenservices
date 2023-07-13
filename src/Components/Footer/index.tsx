97607cdc3eac1540d56a3eaefaeff3214fa31555
ujjwal <ujjwal9131@gmail.com>
2023-07-13 17:09:08 +0530

    changes2.1

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -23,11 +23,11 @@ const Footer = () => {
           
         <div className=" px-6 mt-4 lg:mt-0">
         <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
-          <a className="text-indigo-500 leading-relaxed">mayur@kitchenchimneyservices.in</a>
+          <a className="text-indigo-500 leading-relaxed">Rasoichimneyservices@gmail.com</a>
         </div>
         <div className=" px-6 mt-4 lg:mt-0">
           <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
-          <p className="leading-relaxed">+91-9730947658,+91-7720004562</p>
+          <p className="leading-relaxed">+91 9730947658,+91 7720004562</p>
         </div>
       </div>
     </div>
