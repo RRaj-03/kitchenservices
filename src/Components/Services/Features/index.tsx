8e4d22894ba82d92e0b7e1b992337d8cb7d9a8ca
rraj-03 <ritikraj1369@gmail.com>
2023-07-15 15:16:26 +0530

    image changes

--- src/Components/Services/Features/index.tsx
+++ src/Components/Services/Features/index.tsx
@@ -1,49 +1,72 @@
-import { ArrowDownTrayIcon, BuildingOffice2Icon, HomeIcon } from '@heroicons/react/20/solid'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import {
+  ArrowDownTrayIcon,
+  WrenchScrewdriverIcon,
+  Cog6ToothIcon,
+} from "@heroicons/react/20/solid";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const featurelist = [{
-  name:'RESIDENTIAL SOLUTIONS',
-  desc:'Quick services for gas stove, chimney, geyser, and cooking range.',
-  icon:HomeIcon
-},
-{
-  name:'COMMERCIAL SOLUTIONS',
-  desc:'Serving all industries with reliable maintenance & repair services.',
-  icon:BuildingOffice2Icon
-},
-{
-  name:'INSTALLATION & TESTING',
-  desc:'End-to-end LPG gas/copper pipeline installation services.',
-  icon:ArrowDownTrayIcon
-}]
+const featurelist = [
+  {
+    name: "RESIDENTIAL SOLUTIONS",
+    desc: "Quick services for gas stove, chimney, geyser, and cooking range.",
+    icon: ArrowDownTrayIcon,
+  },
+  {
+    name: "COMMERCIAL SOLUTIONS",
+    desc: "Serving all industries with reliable maintenance & repair services.",
+    icon: WrenchScrewdriverIcon,
+  },
+  {
+    name: "INSTALLATION & TESTING",
+    desc: "End-to-end LPG gas/copper pipeline installation services.",
+    icon: Cog6ToothIcon,
+  },
+];
 const ServiceFeature = () => {
   return (
     <section className="text-gray-600 body-font">
       <div className="mx-auto max-w-2xl lg:text-center pt-32 px-16">
-        <h2 className="text-base font-semibold leading-7 text-indigo-300">Kitchen Chimney Services</h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-200 sm:text-4xl">
-           Featured Works
-          </p>
-          <p className="mt-6 text-lg leading-8 text-gray-300">
-          We are working on different scales to provide every Customer best Service, So that they can enjoy their lifestyle.
-          </p>
-        </div>
-  <div className="container px-5 py-24 mx-auto">
-    <div className="flex flex-wrap -m-4">
-      {featurelist.map((feature,index)=>{
-        return <div className="p-4 w-full md:w-1/3">
-        <div className="h-full border-2 border-gray-200 bg-slate-100 border-opacity-60 rounded-lg overflow-hidden">
-          <Image className="bg-gray-300 lg:h-48 md:h-36 w-full object-cover object-center" height={144} width={200} src={"/Images/Service_feature/"+(index+1)+".jpg"} alt={"Feature"+(index+1)}/>
-          <div className='rounded-full bg-gray-300 inline-block absolute -translate-y-6 translate-x-4'>
-          <feature.icon className="h-12 w-12 p-2 text-indigo-500  " aria-hidden="true"/>
-          </div>
-          <div className="p-6 mt-6">
-            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Featured Works</h2>
-            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">{feature?.name}</h1>
-            <p className="leading-relaxed mb-3">{feature?.desc}</p>
-            {/* <div className="flex items-center flex-wrap ">
+        <h2 className="text-base font-semibold leading-7 text-indigo-300">
+          Kitchen Chimney Services
+        </h2>
+        <p className="mt-2 text-3xl font-bold tracking-tight text-gray-200 sm:text-4xl">
+          Featured Works
+        </p>
+        <p className="mt-6 text-lg leading-8 text-gray-300">
+          We are working on different scales to provide every Customer best
+          Service, So that they can enjoy their lifestyle.
+        </p>
+      </div>
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          {featurelist.map((feature, index) => {
+            return (
+              <div className="p-4 w-full md:w-1/3">
+                <div className="h-full border-2 border-gray-200 bg-slate-100 border-opacity-60 rounded-lg overflow-hidden">
+                  <Image
+                    className="bg-gray-300 lg:h-48 md:h-36 w-full object-cover object-center"
+                    height={144}
+                    width={200}
+                    src={"/Images/Service_feature/" + (index + 1) + ".jpg"}
+                    alt={"Feature" + (index + 1)}
+                  />
+                  <div className="rounded-full bg-gray-300 inline-block absolute -translate-y-6 translate-x-4">
+                    <feature.icon
+                      className="h-12 w-12 p-2 text-indigo-500  "
+                      aria-hidden="true"
+                    />
+                  </div>
+                  <div className="p-6 mt-6">
+                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                      Featured Works
+                    </h2>
+                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                      {feature?.name}
+                    </h1>
+                    <p className="leading-relaxed mb-3">{feature?.desc}</p>
+                    {/* <div className="flex items-center flex-wrap ">
               <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                 <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M5 12h14"></path>
@@ -62,15 +85,15 @@ const ServiceFeature = () => {
                 </svg>6
               </span>
             </div> */}
-          </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-      })}
-     
-    </div>
-  </div>
-</section>
-  )
-}
+    </section>
+  );
+};
 
-export default ServiceFeature
+export default ServiceFeature;
--- src/app/api/Services/route.ts
+++ src/app/api/Services/route.ts
@@ -3,9 +3,7 @@ import clientPromise from "../MongoConnect/route";
 export async function POST(request: Request): Promise<Response> {
   try {
     const client = await clientPromise;
-    console.log("hea");
     const Services = client.db("KitchenServices").collection("Services");
-    console.log("Services", Services);
     const value = await Services.find().toArray();
     return NextResponse.json({ value: value });
   } catch (error) {
