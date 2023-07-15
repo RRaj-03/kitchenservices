626d0872017ad65c1658e05f26afd49619d6b348
rraj-03 <ritikraj1369@gmail.com>
2023-07-15 12:55:34 +0530

    commit

--- src/app/api/Services/route.ts
+++ src/app/api/Services/route.ts
@@ -1,14 +1,15 @@
 import { NextResponse } from "next/server";
 import clientPromise from "../MongoConnect/route";
 
-export async function GET(request:Request) {
-    try {
-      const client = await clientPromise;
-      const Services = client.db("KitchenServices").collection("Services")
-      const value = await Services.find().toArray()
-      return NextResponse.json({"value":value})
-    } catch (error) {
-      console.error(error)
-      return NextResponse.json({error})
-    }
+export async function GET(request: Request) {
+  try {
+    console.log(request.url);
+    const client = await clientPromise;
+    const Services = client.db("KitchenServices").collection("Services");
+    const value = await Services.find().toArray();
+    return NextResponse.json({ value: value });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error });
+  }
 }
