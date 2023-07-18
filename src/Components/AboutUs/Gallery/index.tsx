dd71f046676c9106438e8bf6dd57719c5e232997
rraj-03 <ritikraj1369@gmail.com>
2023-07-18 06:59:37 +0530

    done

--- src/Components/AboutUs/Gallery/index.tsx
+++ src/Components/AboutUs/Gallery/index.tsx
@@ -1,7 +1,134 @@
+"use client";
+import useKitchenStore from "@/Helpers/Store/KitchenStore";
+import Image from "next/image";
 import React from "react";
 // Import Swiper React components
 
+let images = [
+  {
+    title: "Image0",
+    desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam`,
+    src: "https://drive.google.com/file/d/1c10VO_jdrDz3IEysx7mZzV8ktc1hqgl2/view?usp=drive_link",
+  },
+  {
+    title: "Image0",
+    desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam`,
+    src: "https://drive.google.com/file/d/1cDS4c9q4HDMNeE8W6rxrfelff2kv3Q4J/view?usp=drive_link",
+  },
+  {
+    title: "Image0",
+    desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam`,
+    src: "https://drive.google.com/file/d/1cLUGFTVCiLFseCzkd4wlQCND71JCRwdI/view?usp=drive_link",
+  },
+  {
+    title: "Image0",
+    desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam`,
+    src: "https://drive.google.com/file/d/1cAXJVp9eBFlvgNAmBFIXbFTLgCk7wMAk/view?usp=drive_link",
+  },
+  {
+    title: "Image0",
+    desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam`,
+    src: "https://drive.google.com/file/d/1bn9zCRWtou5kpgmVZRSzaWYzKFShVDV7/view?usp=drive_link",
+  },
+
+  {
+    title: "Image0",
+    desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam`,
+    src: "https://drive.google.com/file/d/1bdTjomRYA79p260SqIF58Nd7JhwLNDDp/view?usp=drive_link",
+  },
+
+  {
+    title: "Image0",
+    desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam`,
+    src: "https://drive.google.com/file/d/1bZlfwfBaNbVdXTPjHGagDJNqtNirrT2k/view?usp=drive_link",
+  },
+
+  {
+    title: "Image0",
+    desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam`,
+    src: "https://drive.google.com/file/d/1bW2eIFMykmNpzX--pOuyFyWlA7sySH1_/view?usp=drive_link",
+  },
+
+  {
+    title: "Image0",
+    desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam`,
+    src: "https://drive.google.com/file/d/1dZ-nXNzk2uwkc5guSmD6w1wVTyWJTqEf/view?usp=drive_link",
+  },
+
+  {
+    title: "Image0",
+    desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam`,
+    src: "https://drive.google.com/file/d/1bURmJfk5h2XRRdqL6IV6zR4Ed0GWYfwP/view?usp=drive_link",
+  },
+
+  {
+    title: "Image0",
+    desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam`,
+    src: "https://drive.google.com/file/d/1dPQKCjcCHKrp5pEaFa3s6QK9gT4Oacgg/view?usp=drive_link",
+  },
+
+  {
+    title: "Image0",
+    desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam`,
+    src: "https://drive.google.com/file/d/1dIz-Ezno1Wcc5sKoiCPcA8LiiquPWMYu/view?usp=drive_link",
+  },
+
+  {
+    title: "Image0",
+    desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam`,
+    src: "https://drive.google.com/file/d/1dF1JbUIAFJIWnu_-Uo_wPGMRLg5vrZ7Z/view?usp=drive_link",
+  },
+  {
+    title: "Image0",
+    desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam`,
+    src: "https://drive.google.com/file/d/1dKjXeaFt6gGuwFWo4hVy1_Ww7XT79uo3/view?usp=drive_link",
+  },
+  {
+    title: "Image0",
+    desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam`,
+    src: "https://drive.google.com/file/d/1dTEJVmfMMsAUWfTzvcf9w05wY_B57TM1/view?usp=drive_link",
+  },
+  {
+    title: "Image0",
+    desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam`,
+    src: "https://drive.google.com/file/d/1dGpZdGYH5rULUyRGqzsALaaIZ25EUy2-/view?usp=drive_link",
+  },
+];
+
 export default function Gallery() {
+  const Imgsrc = useKitchenStore((state) => state.ImageString);
   return (
     <div className="  pt-16 pb-4 " id="Gallery">
       {/* <div className='text-5xl text-black font-semibold text-center mb-16'>Gallery</div> */}
@@ -84,126 +211,31 @@ export default function Gallery() {
         </SwiperSlide>
       </Swiper> */}
       <div className="flex flex-wrap ">
-        <div className="w-full px-4 mb-4 lg:w-2/5 ">
-          <div className="relative overflow-hidden shadow-lg group">
-            <img
-              src="https://drive.google.com/file/d/1c10VO_jdrDz3IEysx7mZzV8ktc1hqgl2/view?usp=drive_link"
-              className="group-hover:origin-center group-hover:scale-105 transition inset-0 object-cover object-center  duration-500 w-full h-[350px]"
-              alt=""
-            />
-            <div className="absolute inset-0 z-0 group-hover:bg-black opacity-60"></div>
-            <div className="absolute hidden p-4 text-center content left-4 bottom-4 right-4 group-hover:block">
-              <div className="mb-2 text-2xl font-semibold text-gray-100 dark:text-white ">
-                Lorem ipsum
+        {images.map((ele: any, index: number) => {
+          return (
+            <div className="w-full px-4 mb-4 lg:w-2/5">
+              <div className="relative overflow-hidden shadow-lg group">
+                <Image
+                  width={300}
+                  height={350}
+                  loader={Imgsrc}
+                  alt={"Gallery" + index}
+                  src={ele.src}
+                  className="group-hover:origin-center group-hover:scale-105 transition inset-0 object-cover object-center  duration-500 w-full h-[350px]"
+                />
+                <div className="absolute inset-0 z-0 group-hover:bg-black opacity-60"></div>
+                <div className="absolute hidden p-4 text-center content left-4 bottom-4 right-4 group-hover:block">
+                  <div className="mb-2 text-2xl font-semibold text-gray-100 dark:text-white ">
+                    {ele.title}
+                  </div>
+                  <h2 className="mb-0 text-sm font-light text-gray-300 dark:text-gray-300 ">
+                    {ele.desc}
+                  </h2>
+                </div>
               </div>
-              <h2 className="mb-0 text-sm font-light text-gray-300 dark:text-gray-300 ">
-                Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam
-              </h2>
             </div>
-          </div>
-        </div>
-        <div className="w-full px-4 mb-8 lg:w-3/5 ">
-          <div className="relative overflow-hidden shadow-lg group">
-            <img
-              src="https://drive.google.com/file/d/1E4H8IWRShR3_hjw2_2XUUem9x2FPDqbP/view?usp=sharing"
-              className="group-hover:origin-center group-hover:scale-105 transition inset-0 object-cover object-center  duration-500 w-full h-[350px]"
-              alt=""
-            />
-            <div className="absolute inset-0 z-0 group-hover:bg-black opacity-60"></div>
-            <div className="absolute hidden p-4 text-center content left-4 bottom-4 right-4 group-hover:block">
-              <div className="mb-2 text-2xl font-semibold text-gray-100 dark:text-white ">
-                Lorem ipsum
-              </div>
-              <h2 className="mb-0 text-sm font-light text-gray-300 dark:text-gray-300 ">
-                Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam
-              </h2>
-            </div>
-          </div>
-        </div>
-        <div className="w-full px-4 mb-8 lg:w-3/5 ">
-          <div className="relative overflow-hidden shadow-lg group">
-            <img
-              src="https://i.postimg.cc/Jhf3wJZs/pexels-visual-tag-mx-2566581.jpg"
-              className="group-hover:origin-center group-hover:scale-105 transition inset-0 object-cover object-center  duration-500 w-full h-[350px]"
-              alt=""
-            />
-            <div className="absolute inset-0 z-0 group-hover:bg-black opacity-60"></div>
-            <div className="absolute hidden p-4 text-center content left-4 bottom-4 right-4 group-hover:block">
-              <div className="mb-2 text-2xl font-semibold text-gray-100 dark:text-white ">
-                Lorem ipsum
-              </div>
-              <h2 className="mb-0 text-sm font-light text-gray-300 dark:text-gray-300 ">
-                Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam
-              </h2>
-            </div>
-          </div>
-        </div>
-        <div className="w-full px-4 mb-8 lg:w-2/5 ">
-          <div className="relative overflow-hidden shadow-lg group">
-            <img
-              src="https://i.postimg.cc/mgrb8Ztg/pexels-pixabay-38271.jpg"
-              className="group-hover:origin-center group-hover:scale-105 transition inset-0 object-cover object-center  duration-500 w-full h-[350px]"
-              alt=""
-            />
-            <div className="absolute inset-0 z-0 group-hover:bg-black opacity-60"></div>
-            <div className="absolute hidden p-4 text-center content left-4 bottom-4 right-4 group-hover:block">
-              <div className="mb-2 text-2xl font-semibold text-gray-100 dark:text-white ">
-                Lorem ipsum
-              </div>
-              <h2 className="mb-0 text-sm font-light text-gray-300 dark:text-gray-300 ">
-                Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam
-              </h2>
-            </div>
-          </div>
-        </div>
-        <div className="w-full px-4 mb-8 lg:w-2/5 ">
-          <div className="relative overflow-hidden shadow-lg group">
-            <img
-              src="https://i.postimg.cc/GtBxvn8V/pexels-thisisengineering-3861958.jpg"
-              className="group-hover:origin-center group-hover:scale-105 transition inset-0 object-cover object-center  duration-500 w-full h-[350px]"
-              alt=""
-            />
-            <div className="absolute inset-0 z-0 group-hover:bg-black opacity-60"></div>
-            <div className="absolute hidden p-4 text-center content left-4 bottom-4 right-4 group-hover:block">
-              <div className="mb-2 text-2xl font-semibold text-gray-100 dark:text-white ">
-                Lorem ipsum
-              </div>
-              <h2 className="mb-0 text-sm font-light text-gray-300 dark:text-gray-300 ">
-                Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam
-              </h2>
-            </div>
-          </div>
-        </div>
-        <div className="w-full px-4 mb-8 lg:w-3/5 ">
-          <div className="relative overflow-hidden shadow-lg group">
-            <img
-              src="https://i.postimg.cc/JnPkrd2d/pexels-marc-mueller-380769.jpg"
-              className="group-hover:origin-center group-hover:scale-105 transition inset-0 object-cover object-center  duration-500 w-full h-[350px]"
-              alt=""
-            />
-            <div className="absolute inset-0 z-0 group-hover:bg-black opacity-60"></div>
-            <div className="absolute hidden p-4 text-center content left-4 bottom-4 right-4 group-hover:block">
-              <div className="mb-2 text-2xl font-semibold text-gray-100 dark:text-white ">
-                Lorem ipsum
-              </div>
-              <h2 className="mb-0 text-sm font-light text-gray-300 dark:text-gray-300 ">
-                Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam
-              </h2>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
